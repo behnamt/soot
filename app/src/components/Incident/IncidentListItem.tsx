973c17cc9c0a5ac74087fb55324f540281bbd0a3
Behnam <behnam.tehrani@turbinekreuzberg.com>
2021-01-08 18:44:07 +0100

    refactor IncidentListItem to use reactAsync instead of async useEffect

--- app/src/components/Incident/IncidentListItem.tsx
+++ app/src/components/Incident/IncidentListItem.tsx
@@ -1,38 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { ListItem, ListItemText } from '@material-ui/core';
+import { Async } from 'react-async';
 import { IFullIncident } from '../../@types/ISoot.types';
 import Skeleton from '@material-ui/lab/Skeleton';
 import { useSoot } from '../../context/Soot';
-import { useHistory } from 'react-router-dom';
 
 export const IncidentListItem: React.FC<{ id: number }> = (props: { id: number }) => {
   const { id } = props;
 
-  const [incident, setIncident] = useState<IFullIncident | null>(null);
-
   const { sootRegistryFacade } = useSoot();
   const history = useHistory();
 
-  useEffect(() => {
-    if (sootRegistryFacade) {
-      (async (): Promise<void> => {
-        setIncident(await sootRegistryFacade.getIncident(id));
-      })();
-    }
-  }, [sootRegistryFacade]);
-
-  return incident ? (
-    <ListItem>
-      <ListItemText
-        primary={incident.name}
-        secondary={incident.date}
-        onClick={(): void => history.push(`incidents/${id}`)}
-      />
-    </ListItem>
-  ) : (
-    <React.Fragment>
-      <Skeleton variant="text" animation="wave" />
-      <Skeleton variant="text" animation="wave" />
-    </React.Fragment>
+  return (
+    <Async
+      promiseFn={(): Promise<IFullIncident> => sootRegistryFacade.getIncident(id)}
+      onReject={(error: Error): void => console.debug(error)}
+    >
+      <Async.Pending>
+        <React.Fragment>
+          <Skeleton variant="text" animation="wave" />
+          <Skeleton variant="text" animation="wave" />
+        </React.Fragment>{' '}
+      </Async.Pending>
+      <Async.Fulfilled>
+        {(incident: IFullIncident): React.ReactNode => (
+          <ListItem>
+            <ListItemText
+              primary={incident.name}
+              secondary={incident.date}
+              onClick={(): void => history.push(`incidents/${id}`)}
+            />
+          </ListItem>
+        )}
+      </Async.Fulfilled>
+      <Async.Rejected>{`could not fetch the incident`}</Async.Rejected>
+    </Async>
   );
 };
