229c02a6723e06f35505027e22cf15aeed2c70f0
Behnam <behnam.tehrani@turbinekreuzberg.com>
2021-01-08 12:48:55 +0100

    fix deploy-contract script

--- tools/deploy-contracts.sh
+++ tools/deploy-contracts.sh
@@ -6,7 +6,7 @@ npx oz add SootRegistry && npx oz push -n development
 contract_address=`npx oz deploy --no-interactive --network development --kind upgradeable SootRegistry`
 npx oz send-tx -n development --method initialize --to "$contract_address"
 
-sed -e "s/^\(CONTRACT_ADDRESS=\s*\).*\$/\1$contract_address/" ../app/.env.local > ../app/.env.tmp
+sed -e "s/^\(REACT_APP_SOOT_REGISTRY_CONTRACT_ADDRESS=\s*\).*\$/\1$contract_address/" ../app/.env.local > ../app/.env.tmp
 mv ../app/.env.tmp ../app/.env.local
 
 
