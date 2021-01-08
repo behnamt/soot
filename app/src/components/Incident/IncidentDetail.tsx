6b32a0251c15a7281bc9c13c28adac06271aead6
Behnam <behnam.tehrani@turbinekreuzberg.com>
2021-01-08 18:40:27 +0100

    refactor incidentDetails to use react-async instead of async useEffect

--- app/src/components/Incident/IncidentDetail.tsx
+++ app/src/components/Incident/IncidentDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FilledInput, Box } from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
 import { IFullIncident } from '../../@types/ISoot.types';
@@ -7,48 +7,71 @@ import { loadDescription } from '../../lib/scripts/loadDescription';
 import { useParams } from 'react-router-dom';
 import { useSoot } from '../../context/Soot';
 import { BingMap } from '../Map/BingMap';
+import { Async } from 'react-async';
 
 export const IncidentDetail: React.FC = () => {
   const { incidentId } = useParams<{ incidentId: string }>();
-  const [incident, setIncident] = useState<IFullIncident>(null);
-  const [description, setDescription] = useState('');
 
   const { account } = useWeb3();
   const { sootRegistryFacade } = useSoot();
 
-  useEffect(() => {
-    if (sootRegistryFacade) {
-      (async (): Promise<void> => {
-        setIncident(await sootRegistryFacade.getIncident(Number(incidentId)));
-      })();
-    }
-  }, [sootRegistryFacade]);
-
-  useEffect(() => {
-    if (account && incident) {
-      (async (): Promise<void> => {
-        setDescription(await loadDescription(account, incident.cid, incident.isEncrypted));
-      })();
-    }
-  }, [account, incident]);
-
-  return incident ? (
-    <Box display="flex" flexDirection="column" p={2}>
-      <FilledInput value={incident.author} />
-      <FilledInput value={incident.name} />
-      <FilledInput value={incident.date} />
-      {description ? <FilledInput value={description} /> : <Skeleton />}
-      <BingMap
-        mapOptions={{
-          center: [incident.lat, incident.lon],
-        }}
-        marks={[{ id: '1', name: incident.name, location: { lat: incident.lat, lng: incident.lon } }]}
-        height="50vh"
-      />
-    </Box>
-  ) : (
-    <Box>
-      <Skeleton />
-    </Box>
+  return (
+    <Async
+      promiseFn={(): Promise<IFullIncident> => sootRegistryFacade.getIncident(Number(incidentId))}
+      onReject={(error: Error): void => console.debug(error)}
+    >
+      <Async.Pending>
+        <Skeleton />
+      </Async.Pending>
+      <Async.Fulfilled>
+        {(incident: IFullIncident): React.ReactNode => (
+          <Box display="flex" flexDirection="column" p={2}>
+            <FilledInput value={incident.author} />
+            <FilledInput value={incident.name} />
+            <FilledInput value={incident.date} />
+            <Async
+              promiseFn={(): Promise<string> => loadDescription(account, incident.cid, incident.isEncrypted)}
+              onReject={(error: Error): void => console.debug(error)}
+            >
+              <Async.Pending>
+                <Skeleton />
+              </Async.Pending>
+              <Async.Fulfilled>
+                {(description: string): React.ReactNode => <FilledInput value={description} />}
+              </Async.Fulfilled>
+              <Async.Rejected>{`could not fetch the description`}</Async.Rejected>
+            </Async>
+            <BingMap
+              mapOptions={{
+                center: [incident.lat, incident.lon],
+              }}
+              marks={[{ id: '1', name: incident.name, location: { lat: incident.lat, lng: incident.lon } }]}
+              height="50vh"
+            />
+          </Box>
+        )}
+      </Async.Fulfilled>
+      <Async.Rejected>{`could not fetch the incident`}</Async.Rejected>
+    </Async>
   );
+
+  // return incident ? (
+  //   <Box display="flex" flexDirection="column" p={2}>
+  //     <FilledInput value={incident.author} />
+  //     <FilledInput value={incident.name} />
+  //     <FilledInput value={incident.date} />
+  //     {description ? <FilledInput value={description} /> : <Skeleton />}
+  //     <BingMap
+  //       mapOptions={{
+  //         center: [incident.lat, incident.lon],
+  //       }}
+  //       marks={[{ id: '1', name: incident.name, location: { lat: incident.lat, lng: incident.lon } }]}
+  //       height="50vh"
+  //     />
+  //   </Box>
+  // ) : (
+  //   <Box>
+  //     <Skeleton />
+  //   </Box>
+  // );
 };
