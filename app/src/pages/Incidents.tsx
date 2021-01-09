3e19fd9dd02fed6c10d179bf679af9ebf9025df8
Behnam <behnam.tehrani@turbinekreuzberg.com>
2021-01-09 11:22:12 +0100

    refactor Incidents to use reactAsync instead of async useEffect

--- app/src/pages/Incidents.tsx
+++ app/src/pages/Incidents.tsx
@@ -1,30 +1,35 @@
-import React, { useState, useEffect, ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 import { useSoot } from '../context/Soot';
 import { useWeb3 } from '../context/Web3';
 import { List } from '@material-ui/core';
 import { IncidentListItem } from '../components/Incident/IncidentListItem';
+import { Async } from 'react-async';
+import { Skeleton } from '@material-ui/lab';
 
 export const Incidents: React.FC = () => {
-  const [list, setList] = useState([]);
-
   const { sootRegistryFacade } = useSoot();
   const { account } = useWeb3();
 
-  useEffect((): void => {
-    (async (): Promise<void> => {
-      if (sootRegistryFacade) {
-        setList(await sootRegistryFacade.getAllIncidentIdsForVictim(account.address));
-      }
-    })();
-  }, [sootRegistryFacade]);
-
   return (
-    <List>
-      {list.map(
-        (id: number): ReactElement => (
-          <IncidentListItem key={id} id={id} />
-        ),
-      )}
-    </List>
+    <Async
+      promiseFn={(): Promise<string[]> => sootRegistryFacade.getAllIncidentIdsForVictim(account.address)}
+      onReject={(error: Error): void => console.debug(error)}
+    >
+      <Async.Pending>
+        <Skeleton animation="wave" />
+      </Async.Pending>
+      <Async.Fulfilled>
+        {(list: string[]): React.ReactNode => (
+          <List>
+            {list.map(
+              (id: string): ReactElement => (
+                <IncidentListItem key={id} id={Number(id)} />
+              ),
+            )}
+          </List>
+        )}
+      </Async.Fulfilled>
+      <Async.Rejected>{`could not fetch the incidents`}</Async.Rejected>
+    </Async>
   );
 };
