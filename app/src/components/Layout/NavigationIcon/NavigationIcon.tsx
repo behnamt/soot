305dd16afa49bf8ea38fdb6f6f72a9de91e0d111
Behnam <behnam.tehrani@turbinekreuzberg.com>
2021-01-10 15:14:12 +0100

    reomve node-sass - using styled-components

--- app/src/components/Layout/NavigationIcon/NavigationIcon.tsx
+++ app/src/components/Layout/NavigationIcon/NavigationIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IconButton, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import './NavigationIcon.scss';
+import styled from 'styled-components';
 
 interface NavigationIconProps {
   link: string;
@@ -9,13 +9,24 @@ interface NavigationIconProps {
   icon: React.ReactNode;
 }
 
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+
+  button {
+    width: 50px;
+    margin: 0 auto;
+  }
+`;
+
 export const NavigationIcon: React.FC<NavigationIconProps> = (props: NavigationIconProps) => {
   const { icon, label, link } = props;
 
   return (
-    <Link to={link} className="navigation-icon">
+    <StyledLink to={link} className="navigation-icon">
       <IconButton>{icon}</IconButton>
       <Typography variant="caption">{label}</Typography>
-    </Link>
+    </StyledLink>
   );
 };
