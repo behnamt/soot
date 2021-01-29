d93631366aa55c16290fd3d5d4e50ceb15d4c130
Behnam <behnam.tehrani@turbinekreuzberg.com>
2021-01-29 15:07:50 +0100

    fix app name in title

--- app/src/components/ContextAwarApp.tsx
+++ app/src/components/ContextAwarApp.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Alert } from '@material-ui/lab';
 import { useWeb3 } from '@contexts/Web3';
 import { useIpfs } from '@contexts/IPFS';
+import { Box, CircularProgress, Typography } from '@material-ui/core';
 
 export const ContextAwareApp: React.FC = ({ children }: { children: React.ReactNode }) => {
   const { web3Instance } = useWeb3();
@@ -10,10 +10,9 @@ export const ContextAwareApp: React.FC = ({ children }: { children: React.ReactN
   return !!web3Instance && !isPending && !!ipfs ? (
     <>{children}</>
   ) : (
-    <>
-      <Alert icon={false} severity="info">
-        Loading...
-      </Alert>
-    </>
+    <Box display="flex" flexDirection="column" alignItems="center" pt={4}>
+      <CircularProgress color="secondary" />
+      <Typography variant="body2">Loading assets...</Typography>
+    </Box>
   );
 };
